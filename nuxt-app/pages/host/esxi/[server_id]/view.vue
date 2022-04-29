d124fee99bd37fc4648c86f4fc7bd27fa5640162
danielv <danielv@danielv.no>
2022-04-29 22:30:08 +0000

    Show esxi VMs from server

--- nuxt-app/pages/host/esxi/[server_id]/view.vue
+++ nuxt-app/pages/host/esxi/[server_id]/view.vue
@@ -1,21 +1,50 @@
 <script setup>
-const route = useRoute()
+const route = useRoute();
 
 const resp = await useFetch(`/api/host/esxi/${route.params.server_id}/view`);
 const server = resp.data;
 const { pending, error, refresh } = resp;
 
-const resp_vms = await useFetch(`/api/host/esxi/${route.params.server_id}/list_vms`);
+const resp_vms = await useFetch(
+  `/api/host/esxi/${route.params.server_id}/list_vms`,
+);
 const vms = resp_vms.data;
+const powerStates = {
+  1: "Running",
+  0: "Stopped",
+  2: "Suspended",
+  unknown: "Unknown",
+};
 </script>
 
 <template>
   <div>
     <!-- {{ server }} -->
     <h2>esxi server overview</h2>
-    {{$route.params.server_id}}
-    {{server}}
+    {{ $route.params.server_id }}
+    {{ server }}
     <h2>vms</h2>
-    {{vms}}
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>name</th>
+            <th>state</th>
+            <th>memory</th>
+            <th>cores</th>
+            <th>id</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="vm in JSON.parse(vms)">
+            <td>{{ vm.Name }}</td>
+            <td>{{ powerStates[vm.PowerState] }}</td>
+            <td>{{ vm.MemoryMB }}</td>
+            <td>{{ vm.NumCpu }}</td>
+            <td>{{ vm.Id }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
--- nuxt-app/server/api/host/esxi/[serverid]/list_vms.ts
+++ nuxt-app/server/api/host/esxi/[serverid]/list_vms.ts
@@ -1,19 +1,50 @@
 import datastore from "~~/server/services/datastore";
 import { exec } from "child_process";
+import { PowerShell } from "node-powershell";
 
-export default defineEventHandler(async (event) => {
+let hasDisabledCertificate = false;
+let hasDisabledCeip = false;
+
+export default defineEventHandler(async (event): Promise<string> => {
   let server = datastore.servers.find(
     (server) => server.id === event.context.params.serverid,
   );
   if (!server) {
     throw new Error("Server not found");
   }
+  const shell = new PowerShell({
+    debug: false,
+    executableOptions: {
+      "-ExecutionPolicy": "Bypass",
+      "-NoProfile": true,
+    },
+  });
   // Run ls shell command
-  let ls = await runShellCommand("ls");
-  return ls;
+  if (!hasDisabledCertificate) {
+    await runShellCommand(
+      `pwsh -c "Set-PowerCLIConfiguration -InvalidCertificateAction:Ignore -Confirm:\\\$false"`,
+    );
+    hasDisabledCertificate = true;
+  }
+  if (!hasDisabledCeip) {
+    await runShellCommand(
+      `pwsh -c "Set-PowerCLIConfiguration -Scope:User -Confirm:\\\$false -ParticipateInCEIP:\\\$false"`,
+    );
+    hasDisabledCeip = true;
+  }
+  // let output = await runShellCommand(`pwsh -c "Connect-VIServer -Server 192.168.10.52 -User root -Password 'Monster123!'; Get-VM"`);
+  await shell.invoke(
+    `Connect-VIServer -Server ${server.ip} -User ${server.username} -Password '${server.password}'`,
+  );
+  let output = await shell.invoke(
+    `Get-VM -Server ${server.ip} | Select-Object -Property Name,Uid,PowerState,NumCpu,CoresPerSocket,MemoryMB,HardwareVersion,PersistendId,GuestId,UsedSpaceGB,ProvisionedSpaceGB,DatastoreIdList,CreateDate,Id | ConvertTo-Json -Compress -Depth 99`,
+  );
+  // await runShellCommand("pwsh -c Get-VM");
+
+  return output.raw.replace("\u001b[?1h\u001b[?1l", "").replace("[?1h[?1l[?1h[?1l", "");
 });
 
-async function runShellCommand(command: string) {
+async function runShellCommand(command: string): Promise<string> {
   return new Promise((resolve, reject) => {
     exec(command, (err, stdout, stderr) => {
       if (err) {
@@ -22,7 +53,7 @@ async function runShellCommand(command: string) {
       }
       // the *entire* stdout and stderr (buffered)
       console.log(`stdout: ${stdout}`);
-    //   console.log(`stderr: ${stderr}`);
+      if (stderr) console.log(`stderr: ${stderr}`);
       resolve(stdout);
     });
   });
