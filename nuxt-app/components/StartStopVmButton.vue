d0dd0a118e1599fba7551d8b68a03e495cb9719b
danielv <danielv@danielv.no>
2022-05-07 17:42:07 +0000

    Added suspend function

--- nuxt-app/components/StartStopVmButton.vue
+++ nuxt-app/components/StartStopVmButton.vue
@@ -19,6 +19,14 @@
       v-if="[1].includes(props.vm?.PowerState)"
       size="small"
       :loading="loading"
+      v-on:click="suspend_vm(props.vm)"
+      danger
+      >Suspend</a-button
+    >
+    <a-button
+      v-if="[1].includes(props.vm?.PowerState)"
+      size="small"
+      :loading="loading"
       v-on:click="stop_vm(props.vm)"
       danger
       >Stop</a-button
@@ -61,6 +69,16 @@ async function stop_vm(vm) {
   loading.value = true;
   const { data: result } = await $fetch(
     `/api/host/esxi/${route.params.server_id}/vm/${props.name}/stop`,
+  );
+  loading.value = false;
+  props.update_vm?.(vm);
+}
+
+async function suspend_vm(vm) {
+  console.log(`Suspend`, vm.Name);
+  loading.value = true;
+  const { data: result } = await $fetch(
+    `/api/host/esxi/${route.params.server_id}/vm/${props.name}/suspend`,
   );
   loading.value = false;
   props.update_vm?.(vm);
