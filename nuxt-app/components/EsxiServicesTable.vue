3eae62f90af118a5d5dc249db202ff64068d433f
danielv <danielv@danielv.no>
2022-05-14 15:26:56 +0000

    Fixed starting and stopping of services

--- nuxt-app/components/EsxiServicesTable.vue
+++ nuxt-app/components/EsxiServicesTable.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-table :dataSource="data" :loading="pending" size="small">
+  <a-table :dataSource="data" :loading="pending || loading" size="small">
     <a-table-column title="Name" dataIndex="Label" key="Label" />
     <a-table-column title="Restart policy" dataIndex="Policy" key="Policy" />
     <a-table-column title="Status" dataIndex="Running" key="Status">
@@ -12,20 +12,23 @@
     <a-table-column title="Actions" dataIndex="undefined" key="buttons">
       <template #default="{ record }">
         <a-dropdown-button>
-          <start-stop-service-button
-            :server_id="route.params.server_id"
-            :service_id="record.Key"
-            :service="record"
-          />
           <template #overlay>
-            <a-menu @click="handleMenuClick">
-              <a-menu-item>
-                <form-outlined />
-                Expand
+            <a-menu @click="(e) => handleMenuClick(e, record)">
+              <a-menu-item key="start">
+                <caret-right-outlined />
+                Start
               </a-menu-item>
-              <a-menu-item>
+              <a-menu-item key="stop">
+                <stop-outlined />
+                Stop
+              </a-menu-item>
+              <a-menu-item key="restart">
                 <reload-outlined />
                 Restart
+              </a-menu-item>
+              <a-menu-item key="expand">
+                <form-outlined />
+                Expand
               </a-menu-item>
             </a-menu>
           </template>
@@ -36,7 +39,12 @@
 </template>
 
 <script setup>
-import { FormOutlined, ReloadOutlined } from "@ant-design/icons-vue";
+import {
+  FormOutlined,
+  ReloadOutlined,
+  StopOutlined,
+  CaretRightOutlined,
+} from "@ant-design/icons-vue";
 const route = useRoute();
 
 const states = {
@@ -50,9 +58,11 @@ const states = {
   },
 };
 
+const loading = ref(false);
+
 // Fetch datastores
 const { data, pending, error, refresh } = useFetch(
-  `/api/host/esxi/${route.params.server_id}/services`,
+  `/api/host/esxi/${route.params.server_id}/service`,
   {
     lazy: true,
     transform: (data) => {
@@ -63,7 +73,45 @@ const { data, pending, error, refresh } = useFetch(
   },
 );
 
-function handleMenuClick(event) {
-  console.log("Menu click: ", event);
+function handleMenuClick(event, record) {
+  // console.log("Menu click: ", event, record);
+  switch (event.key) {
+    case "start":
+      console.log("Start: ", record);
+      start_service(record);
+      break;
+    case "stop":
+      console.log("Stop: ", record);
+      stop_service(record);
+      break;
+    case "expand":
+      console.log("Expand: ", record);
+      expand_service(record);
+      break;
+    case "restart":
+      console.log("Restart: ", record);
+      restart_service(record);
+      break;
+  }
+}
+
+async function start_service(service) {
+  console.log(`Starting`, service.Key);
+  loading.value = true;
+  const { data: result } = await $fetch(
+    `/api/host/esxi/${route.params.server_id}/service/${service.Key}/start`,
+  );
+  loading.value = false;
+  refresh();
+}
+
+async function stop_service(service) {
+  console.log(`Stopping`, service.Key);
+  loading.value = true;
+  const { data: result } = await $fetch(
+    `/api/host/esxi/${route.params.server_id}/service/${service.Key}/stop`,
+  );
+  loading.value = false;
+  refresh();
 }
 </script>
